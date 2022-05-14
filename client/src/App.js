ef5a608178f0211fc92edc4d68da0ddf2cd9f70a
Zihao Jin <zihaojin@Zihaos-MacBook-Air.local>
2022-05-14 03:07:07 -0400

    added functionality to display all employee information.

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,9 @@ function App() {
   const [position, setPosition] = useState("");
   const [wage, setWage] = useState(0);
 
+  const [employeeList, setEmployeeList] = useState([]);
+
   const addEmployee = () => {
-    console.log(name);
     Axios.post("http://localhost:3001/create", {
       name: name,
       age: age,
@@ -18,7 +19,22 @@ function App() {
       position: position,
       wage: wage,
     }).then(() => {
-      console.log("success!");
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          citizenship: citizenship,
+          position: position,
+          wage: wage,
+        },
+      ]);
+    });
+  };
+
+  const getEmployees = () => {
+    Axios.get("http://localhost:3001/employees").then((response) => {
+      setEmployeeList(response.data);
     });
   };
 
@@ -71,6 +87,24 @@ function App() {
           required
         />
         <button onClick={addEmployee}>Add Employee</button>
+      </div>
+      ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+      <div className="employees">
+        <button onClick={getEmployees}>Show Emplyees</button>
+
+        {employeeList.map((val, key) => {
+          return (
+            <div className="employee">
+              <div>
+                <h3>Name: {val.name}</h3>
+                <h3>Age: {val.age}</h3>
+                <h3>Citizenship: {val.citizenship}</h3>
+                <h3>Position: {val.position}</h3>
+                <h3>Wage: {val.wage}</h3>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -33,6 +33,16 @@ app.post("/create", (req, res) => {
   );
 });
 
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM Employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
 app.listen(3001, () => {
   console.log("Your server is running on port 3001");
 });
